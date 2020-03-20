c30b125a59d81deeb2c2f5a8374100c1c6e5060e
Seán Coll <seancoll@Macbook-Air-of-Sean.local>
2020-03-20 12:02:11 +0000

    Added parabola

--- java/src/C18332623/Startup.java
+++ java/src/C18332623/Startup.java
@@ -17,6 +17,14 @@ public class Startup extends PApplet
     AudioOutput out;
     Oscil wave;
 
+    float oscilAmp = 500;
+    float lineX = 0;
+    float lineY = 450;
+    float offset = 0.1f;
+    float parabTop = 250;
+    int count = 0;
+    boolean halfParab = false;
+
     public void settings()
     {
         size(500,500);
@@ -27,12 +35,67 @@ public class Startup extends PApplet
         minim = new Minim(this);
 
         out = minim.getLineOut();
-        wave = new Oscil(500, 0.5f, Waves.SINE);
+        wave = new Oscil(oscilAmp, 0.5f, Waves.SINE);
         wave.patch(out);
+        background(0);
+        colorMode(HSB);
+        strokeWeight(10);
     }
 
     public void draw()
     {
-        background(0);
+        // if(frameCount % 2 == 0)
+        // {
+        //     drawParabola();
+        // }
+        if(count <= width)
+        {
+            drawParabola();
+        }
+        if(count >= width)
+        {
+            minim.stop();
+        }
+        
+        
+    }
+
+    public void drawParabola()
+    {
+        float lineCol = map(oscilAmp, 500, 1250, 0, 255);
+        stroke(lineCol, 255, 255);
+
+        if(halfParab == false)
+        {
+            beginShape();
+            vertex(lineX,lineY);
+            lineX++;
+            lineY = map(oscilAmp, 500, 1250, 450, 50);
+            oscilAmp += 1 + offset;
+            wave.setFrequency(oscilAmp);
+            vertex(lineX,lineY);
+            endShape();
+            offset += 0.01f;
+        }
+        
+        
+        if(halfParab == true)
+        {
+            beginShape();
+            vertex(lineX,lineY);
+            lineX++;
+            lineY = map(oscilAmp, 500, 1250, 450, 50);
+            oscilAmp -= 1 + offset;
+            wave.setFrequency(oscilAmp);
+            vertex(lineX,lineY);
+            endShape();
+            offset -= 0.01f;
+        }
+        count ++;
+        if(count == parabTop)
+        {
+            halfParab = true;
+        }
+        
     }
 }
