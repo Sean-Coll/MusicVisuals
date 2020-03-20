dade2e0811670ef3c6d27b5a26a243996774771f
Seán Coll <seancoll@Macbook-Air-of-Sean.local>
2020-03-20 10:05:46 +0000

    Initial commit

---
+++ java/src/C18332623/Startup.java
@@ -0,0 +1,12 @@
+package C18332623;
+
+import ie.tudublin.Visual;
+
+/*  This class will run a startup sequence complete with loading bar,
+    parabolic frequency test and colour test
+*/
+
+class Startup
+{
+    
+}
